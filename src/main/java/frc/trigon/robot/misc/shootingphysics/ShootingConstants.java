9e2ce5df5db624214b484659880089f57e2b22c0
Yishai Levy <96019039+levyishai@users.noreply.github.com>
2026-01-15 02:17:55 +0200

    Multiple timestamps

--- src/main/java/frc/trigon/robot/misc/shootingphysics/ShootingConstants.java
+++ src/main/java/frc/trigon/robot/misc/shootingphysics/ShootingConstants.java
@@ -5,28 +5,28 @@ import frc.trigon.lib.utilities.flippable.FlippableTranslation2d;
 public class ShootingConstants {
     public static final ShootingPolynomial
             SHOOTING_VELOCITY_MPS_POLYNOMIAL = new ShootingPolynomial(
-            4.767958996031e+00,
-            -7.728484623310e-02,
-            4.251929181565e-01,
-            1.040608899068e-01,
-            -1.983523275604e-01,
-            7.486351577742e-02,
-            -4.191789774152e-04,
-            -9.365805191577e-03,
-            1.565699698698e-02,
-            -5.277858971999e-03
+            5.146747052198e+00,
+            -1.643594256333e-02,
+            3.450899319299e-01,
+            9.507680110304e-02,
+            -1.874843832199e-01,
+            8.292659454650e-02,
+            -2.198145568214e-03,
+            -5.599197896374e-03,
+            1.447610537365e-02,
+            -5.717921526029e-03
     ),
             HOOD_ANGLE_RADIANS_POLYNOMIAL = new ShootingPolynomial(
-                    1.623183036320e+00,
-                    2.955241202250e-01,
-                    -3.475512327334e-01,
-                    3.482802254703e-02,
-                    -8.015586879455e-02,
-                    5.575569393422e-02,
-                    9.848403107275e-04,
-                    -3.802057779430e-03,
-                    6.113602843587e-03,
-                    -3.030190410998e-03
+                    1.607273351413e+00,
+                    2.571395162932e-01,
+                    -2.879842443021e-01,
+                    2.020244109890e-02,
+                    -5.508424076508e-02,
+                    4.455905420498e-02,
+                    -6.923840792824e-04,
+                    -1.315012045184e-03,
+                    4.085003609577e-03,
+                    -2.473243914566e-03
             );
 
     public static final FlippableTranslation2d HUB_POSITION = new FlippableTranslation2d(4.636, 4, true); // TODO: Update to actual hub position
