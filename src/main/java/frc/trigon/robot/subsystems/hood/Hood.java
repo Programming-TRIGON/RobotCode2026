b9ddf74aee9c1a672562f748d67ff1e6cd176b58
Yishai Levy <96019039+levyishai@users.noreply.github.com>
2026-01-20 03:25:59 +0200

    Implement shooting position

--- src/main/java/frc/trigon/robot/subsystems/hood/Hood.java
+++ src/main/java/frc/trigon/robot/subsystems/hood/Hood.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.trigon.lib.hardware.phoenix6.cancoder.CANcoderEncoder;
 import frc.trigon.lib.hardware.phoenix6.talonfx.TalonFXMotor;
 import frc.trigon.lib.hardware.phoenix6.talonfx.TalonFXSignal;
-import frc.trigon.robot.RobotContainer;
 import frc.trigon.robot.misc.shootingphysics.ShootingCalculations;
 import frc.trigon.robot.subsystems.MotorSubsystem;
 import org.littletonrobotics.junction.Logger;
@@ -51,17 +50,10 @@ public class Hood extends MotorSubsystem {
 
     @Override
     public void updateMechanism() {
-        final Rotation2d currentAngle = getCurrentAngle();
-        final Rotation2d targetProfiledAngle = Rotation2d.fromRotations(motor.getSignal(TalonFXSignal.CLOSED_LOOP_REFERENCE));
         HoodConstants.MECHANISM.update(
-                currentAngle,
-                targetProfiledAngle
-        );
+                getCurrentAngle(),
+                Rotation2d.fromRotations(motor.getSignal(TalonFXSignal.CLOSED_LOOP_REFERENCE) + HoodConstants.POSITION_OFFSET_FROM_GRAVITY_OFFSET_ROTATION));
         Logger.recordOutput("Poses/Components/HoodPose", calculateVisualizationPose());
-
-        Logger.recordOutput("Hood/TargetAngleDegrees", targetAngle.getDegrees());
-        Logger.recordOutput("Hood/CurrentAngleDegrees", currentAngle.getDegrees());
-        Logger.recordOutput("Hood/TargetProfiledAngleDegrees", targetProfiledAngle.getDegrees());
     }
 
     @Override
@@ -88,7 +80,8 @@ public class Hood extends MotorSubsystem {
     }
 
     public Rotation2d getCurrentAngle() {
-        return Rotation2d.fromRotations(motor.getSignal(TalonFXSignal.POSITION));
+        final double offsettedCurrentAngleRotations = motor.getSignal(TalonFXSignal.POSITION) + HoodConstants.POSITION_OFFSET_FROM_GRAVITY_OFFSET_ROTATION;
+        return Rotation2d.fromRotations(offsettedCurrentAngleRotations);
     }
 
     void aimAtHub() {
@@ -106,15 +99,15 @@ public class Hood extends MotorSubsystem {
 
     void setTargetAngle(Rotation2d targetAngle) {
         this.targetAngle = targetAngle;
-        motor.setControl(positionRequest.withPosition(targetAngle.getRotations()));
+        final double offsettedTargetAngleRotations = targetAngle.getRotations() - HoodConstants.POSITION_OFFSET_FROM_GRAVITY_OFFSET_ROTATION;
+        motor.setControl(positionRequest.withPosition(offsettedTargetAngleRotations));
     }
 
     private Pose3d calculateVisualizationPose() {
-        final Pose3d turretPose = RobotContainer.TURRET.calculateVisualizationPose();
-        final Transform3d hoodTransform = new Transform3d(
-                new Translation3d(),
-                new Rotation3d(0, -getCurrentAngle().getRadians(), 0)
+        final Transform3d pitchTransform = new Transform3d(
+                new Translation3d(0, 0, 0),
+                new Rotation3d(0, getCurrentAngle().getRadians(), 0)//TODO implement turret rotation
         );
-        return turretPose.plus(HoodConstants.TURRET_TO_HOOD_OFFSET).plus(hoodTransform);
+        return HoodConstants.HOOD_VISUALIZATION_ORIGIN_POINT.transformBy(pitchTransform);
     }
 }
