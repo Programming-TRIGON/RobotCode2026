1c3fb110bc178a8a0ef6c6b324ebb269622b2c89
Yishai Levy <96019039+levyishai@users.noreply.github.com>
2026-01-20 02:38:39 +0200

    nobody'll notice

--- src/main/java/frc/trigon/robot/subsystems/loader/LoaderCommands.java
+++ src/main/java/frc/trigon/robot/subsystems/loader/LoaderCommands.java
@@ -6,14 +6,11 @@ import frc.trigon.lib.commands.ExecuteEndCommand;
 import frc.trigon.lib.commands.NetworkTablesCommand;
 import frc.trigon.robot.RobotContainer;
 
-import java.util.Set;
-
 public class LoaderCommands {
     public static Command getDebuggingCommand() {
         return new NetworkTablesCommand(
-                RobotContainer.LOADER::setTargetVelocity,
+                LoaderCommands::getSetTargetVelocityCommand,
                 false,
-                Set.of(RobotContainer.LOADER),
                 "Debugging/LoaderTargetVelocityMetersPerSecond"
         );
     }
