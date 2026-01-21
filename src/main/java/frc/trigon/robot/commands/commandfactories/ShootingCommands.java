f91e160ebca3394d4578f099bef89bf1d66cc703
YinonError404 <ynnon.dahan@gmail.com>
2026-01-21 17:34:55 +0200

    Initial Commit

---
+++ src/main/java/frc/trigon/robot/commands/commandfactories/ShootingCommands.java
@@ -0,0 +1,40 @@
+package frc.trigon.robot.commands.commandfactories;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.trigon.robot.misc.shootingphysics.ShootingCalculations;
+import frc.trigon.robot.subsystems.hood.HoodCommands;
+import frc.trigon.robot.subsystems.loader.Loader;
+import frc.trigon.robot.subsystems.loader.LoaderCommands;
+import frc.trigon.robot.subsystems.loader.LoaderConstants;
+import frc.trigon.robot.subsystems.shooter.ShooterCommands;
+import frc.trigon.robot.subsystems.spindexer.SpindexerCommands;
+import frc.trigon.robot.subsystems.spindexer.SpindexerConstants;
+import frc.trigon.robot.subsystems.turret.TurretCommands;
+
+public class ShootingCommands {
+    public static Command getAimAtHubCommand() {
+        return new ParallelCommandGroup(
+                getUpdateShootingCalculations(),
+                TurretCommands.getAlignToHubCommand(),
+                HoodCommands.getAimAtHubCommand(),
+                ShooterCommands.getAimAtHubCommand()
+        );
+    }
+
+    public static Command getUpdateShootingCalculations() {
+        return new RunCommand(
+                ShootingCalculations.getInstance()::updateCalculations
+        );
+    }
+
+    public static Command getShootFuelCommand() {
+        return new SequentialCommandGroup(
+                getAimAtHubCommand(),
+                SpindexerCommands.getSetTargetStateCommand(SpindexerConstants.SpindexerState.FEED_TO_TURRET),
+                LoaderCommands.getSetTargetStateCommand(LoaderConstants.LoaderState.LOAD)
+        );
+    }
+}
