2c1ec49437e228c3afc8ccc0b39ad1441ff00cd3
YinonError404 <ynnon.dahan@gmail.com>
2026-01-21 17:50:39 +0200

    cleaner

--- src/main/java/frc/trigon/robot/commands/commandfactories/ShootingCommands.java
+++ src/main/java/frc/trigon/robot/commands/commandfactories/ShootingCommands.java
@@ -3,7 +3,6 @@ package frc.trigon.robot.commands.commandfactories;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.trigon.robot.misc.shootingphysics.ShootingCalculations;
 import frc.trigon.robot.subsystems.hood.HoodCommands;
 import frc.trigon.robot.subsystems.loader.LoaderCommands;
