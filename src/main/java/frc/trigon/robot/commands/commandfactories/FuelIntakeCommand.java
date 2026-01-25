b007c7bc32adf11c572b9d8a634c206f2e45a28d
0xRSAvoid <noambenhaim02@gmail.com>
2026-01-25 22:16:20 +0200

    changed command

--- src/main/java/frc/trigon/robot/commands/commandfactories/FuelIntakeCommand.java
+++ src/main/java/frc/trigon/robot/commands/commandfactories/FuelIntakeCommand.java
@@ -1,6 +1,9 @@
 package frc.trigon.robot.commands.commandfactories;
 
-import edu.wpi.first.wpilibj2.command.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.trigon.robot.RobotContainer;
 import frc.trigon.robot.commands.commandclasses.IntakeAssistCommand;
 import frc.trigon.robot.constants.OperatorConstants;
@@ -8,38 +11,18 @@ import frc.trigon.robot.subsystems.intake.IntakeCommands;
 import frc.trigon.robot.subsystems.intake.IntakeConstants;
 
 public class FuelIntakeCommand {
-/*    public static Command getToggleIntakePreparedCommand() {
-        return new InstantCommand(() -> OperatorConstants.SHOULD_KEEP_INTAKE_OPEN = !OperatorConstants.SHOULD_KEEP_INTAKE_OPEN);
-    }
-
-    *//** Default command: continuously applies PREPARE_TO_INTAKE or REST based on the flag *//*
-    public static Command getIntakePreparedDefaultCommand() {
-        return Commands.either(
-                IntakeCommands.getSetTargetStateCommand(IntakeConstants.IntakeState.PREPARE_TO_INTAKE),
-                IntakeCommands.getSetTargetStateCommand(IntakeConstants.IntakeState.REST),
-                () -> OperatorConstants.SHOULD_KEEP_INTAKE_OPEN
-        ).repeatedly();
-    }*/
-
-    public static Command getToggleShouldKeepIntakeOpenCommand() {
-        return new InstantCommand(() -> {
-        }).andThen(
-                new ConditionalCommand(
-                        new InstantCommand(() ->
-                                RobotContainer.INTAKE.changeDefaultCommand(
-                                        IntakeCommands.getSetTargetStateCommand(IntakeConstants.IntakeState.PREPARE_TO_INTAKE)
-                                )
-                        ),
-                        new InstantCommand(() ->
-                                RobotContainer.INTAKE.changeDefaultCommand(
-                                        IntakeCommands.getSetTargetStateCommand(IntakeConstants.IntakeState.REST)
-                                )
-                        ),
-                        OperatorConstants.SHOULD_KEEP_INTAKE_OPEN::getAsBoolean
-                )
+    public static Command getToggleShouldKeepIntakeOpenCommand(){
+        return new ConditionalCommand(
+                new InstantCommand(
+                        () -> RobotContainer.INTAKE.changeDefaultCommand(IntakeCommands.getSetTargetStateCommand(IntakeConstants.IntakeState.PREPARE_TO_INTAKE))
+                ),
+                new InstantCommand(
+                        () -> RobotContainer.INTAKE.changeDefaultCommand(IntakeCommands.getSetTargetStateCommand(IntakeConstants.IntakeState.REST))
+                ),
+                OperatorConstants.SHOULD_KEEP_INTAKE_OPEN
         );
     }
-
+    
     public static Command getIntakeCommand() {
         return new ParallelCommandGroup(
                 IntakeCommands.getSetTargetStateCommand(IntakeConstants.IntakeState.INTAKE),
