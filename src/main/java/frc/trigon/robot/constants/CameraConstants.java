77310752cef9cb3d9fc876352f8218d301217540
ShmayaR <shmaya.rosenblatt@gmail.com>
2026-01-26 20:24:38 +0200

    added calculation for turret camera pose

--- src/main/java/frc/trigon/robot/constants/CameraConstants.java
+++ src/main/java/frc/trigon/robot/constants/CameraConstants.java
@@ -4,7 +4,12 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
+import frc.trigon.robot.misc.TurretCameraCalculations;
 import frc.trigon.robot.misc.objectdetection.objectdetectioncamera.ObjectDetectionCamera;
+import frc.trigon.robot.poseestimation.apriltagcamera.AprilTagCamera;
+import frc.trigon.robot.poseestimation.apriltagcamera.AprilTagCameraConstants;
+import frc.trigon.robot.poseestimation.apriltagcamera.DynamicCameraTransform;
+import frc.trigon.robot.poseestimation.robotposeestimator.StandardDeviations;
 
 public class CameraConstants {
     private static final Transform3d ROBOT_CENTER_TO_OBJECT_DETECTION_CAMERA = new Transform3d(//TODO: AHAHAHAHAH
@@ -12,9 +17,16 @@ public class CameraConstants {
             new Rotation3d(0, Units.degreesToRadians(30), 0)
     );
 
+    private static final DynamicCameraTransform ROBOT_CENTER_TO_HUB_TAG_CAMERA = new DynamicCameraTransform(timeStamp -> TurretCameraCalculations.CAMERA_TO_ROBOT());
+
     public static final double OBJECT_POSE_ESTIMATOR_DELETION_THRESHOLD_SECONDS = 0.5;
     public static final ObjectDetectionCamera OBJECT_DETECTION_CAMERA = new ObjectDetectionCamera(
             "ObjectDetectionCamera",
             ROBOT_CENTER_TO_OBJECT_DETECTION_CAMERA
     );
+
+    public static final AprilTagCamera HUB_CAMERA = new AprilTagCamera(
+            AprilTagCameraConstants.AprilTagCameraType.PHOTON_CAMERA,
+            "HubTagCamera", ROBOT_CENTER_TO_HUB_TAG_CAMERA,
+            new StandardDeviations(1, 1));
 }
