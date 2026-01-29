5bd7c3da59d031e2c29b433e7320a168ee70b40a
Yishai Levy <96019039+levyishai@users.noreply.github.com>
2026-01-29 23:34:42 +0200

    Shooting Commands (#36)
    * Create ShootingCommands.java
    
    * Commit while it's all working
    
    * Added shoot from set position
    
    * Small refactoring + tiny fix (which needs changes)
    
    * Quick refactor
    
    * Added wide check for turret
    
    * Should shoot logic fix + cleaning
    
    * UseWideTolerance
    
    * Implement proper constant for hub pose
    
    * Oops
    
    * Extract alliance zone check into function
    
    * Alliance zone length clarification
    
    * Set position to Fixed position
    
    * Store Shooting Calcs as constant
    
    * Add todo
    
    * Fix shooting readiness chec
    
    * Very debatable newline, but it's too late for me to have strength to debate about it so I'll just leave it and may bring up my thoughts another time
    
    * Also debatable, but probably the right decision
    
    * Update shooting_lut.bin
    
    * Calibrate and add  delivery logic and calcs
    
    * Change some setpoints
    
    * Made stop in turret reset the target
    
    * Some stuff
    
    * Extract and fix hub aim check for shooter
    
    * Update Turret.java
    
    * Things work
    
    * Create variable in shooter for shooting at hub check
    
    * Pitcher -> Hood
    
    * Update via new controls
    
    * Final
    
    * Added all triggers
    
    * Fix intake commands/controls/assist
    
    * Formatting comments
    
    * Refactor
    
    * deleted new line and extra spaces
    
    * cleaner code
    
    * Logic cleanup
    
    * Added operator buttons
    
    * Proxy
    
    * Generalize fixed delivery
    
    * Perhaps is teleop is better
    
    ---------
    
    Co-authored-by: Strflightmight09 <148347057+Strflightmight09@users.noreply.github.com>
    Co-authored-by: Trigon5990 <Trigon5990@gmail.com>
    Co-authored-by: 0xRSAvoid <noambenhaim02@gmail.com>

--- src/main/java/frc/trigon/robot/constants/FieldConstants.java
+++ src/main/java/frc/trigon/robot/constants/FieldConstants.java
@@ -7,7 +7,6 @@ import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation2d;
 import frc.trigon.lib.utilities.FilesHandler;
 import frc.trigon.lib.utilities.flippable.FlippableTranslation2d;
 
@@ -19,20 +18,25 @@ public class FieldConstants {
     public static final double
             FIELD_WIDTH_METERS = FlippingUtil.fieldSizeY,
             FIELD_LENGTH_METERS = FlippingUtil.fieldSizeX;
+
     private static final List<Integer> I_HATE_YOU = List.of(
             //Tags to ignore
     );
-
-    public static final FlippableTranslation2d
-            LEFT_DELIVERY_POSITION = new FlippableTranslation2d(new Translation2d(1.7175, 1.76), true),
-            RIGHT_DELIVERY_POSITION = new FlippableTranslation2d(new Translation2d(1.7175, 7.065), true);
-
     private static final boolean SHOULD_USE_HOME_TAG_LAYOUT = false;
     public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT = createAprilTagFieldLayout();
     private static final Transform3d TAG_OFFSET = new Transform3d(0, 0, 0, new Rotation3d(0, 0, 0));
     public static final HashMap<Integer, Pose3d> TAG_ID_TO_POSE = fieldLayoutToTagIDToPoseMap();
 
-    public static final FlippableTranslation2d HUB_POSITION = new FlippableTranslation2d(4.636, 4, true); // TODO: Update to actual hub position
+    private static final double
+            BLUE_RELATIVE_DELIVERY_POSITION_X = 3,
+            DELIVERY_POSITION_Y_OFFSET_FROM_CENTER_METERS = 2.2;
+    public static final FlippableTranslation2d
+            HUB_POSITION = new FlippableTranslation2d(4.625594, FIELD_WIDTH_METERS / 2, true),
+            RIGHT_DELIVERY_POSITION = new FlippableTranslation2d(BLUE_RELATIVE_DELIVERY_POSITION_X, (FIELD_WIDTH_METERS / 2) - DELIVERY_POSITION_Y_OFFSET_FROM_CENTER_METERS, true),
+            LEFT_DELIVERY_POSITION = new FlippableTranslation2d(BLUE_RELATIVE_DELIVERY_POSITION_X, (FIELD_WIDTH_METERS / 2) + DELIVERY_POSITION_Y_OFFSET_FROM_CENTER_METERS, true);
+    public static final double
+            ALLIANCE_ZONE_LENGTH = 4.625594,
+            DELIVERY_ZONE_START_BLUE_X = ALLIANCE_ZONE_LENGTH + 0.5;
 
     private static AprilTagFieldLayout createAprilTagFieldLayout() {
         try {
