08cca2a9e69523681159794134e48ec77505bb8f
Yishai Levy <96019039+levyishai@users.noreply.github.com>
2026-02-04 03:23:01 +0200

    unerrornous

--- src/main/java/frc/trigon/robot/constants/CameraConstants.java
+++ src/main/java/frc/trigon/robot/constants/CameraConstants.java
@@ -1,26 +1,17 @@
 package frc.trigon.robot.constants;
 
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.util.Units;
+import frc.trigon.robot.RobotContainer;
 import frc.trigon.robot.misc.objectdetection.objectdetectioncamera.ObjectDetectionCamera;
 import frc.trigon.robot.poseestimation.apriltagcamera.AprilTagCamera;
 import frc.trigon.robot.poseestimation.apriltagcamera.AprilTagCameraConstants;
 import frc.trigon.robot.poseestimation.apriltagcamera.DynamicCameraTransform;
 import frc.trigon.robot.poseestimation.robotposeestimator.StandardDeviations;
-import frc.trigon.robot.subsystems.turret.TurretCameraTransformCalculator;
 
 public class CameraConstants {
-    private static final Transform3d ROBOT_CENTER_TO_OBJECT_DETECTION_CAMERA = new Transform3d(//TODO: Find actual values
-            new Translation3d(0, 0, 0.8),
-            new Rotation3d(0, Units.degreesToRadians(30), 0)
-    );
-
     public static final double OBJECT_POSE_ESTIMATOR_DELETION_THRESHOLD_SECONDS = 0.5;
     public static final ObjectDetectionCamera OBJECT_DETECTION_CAMERA = new ObjectDetectionCamera(
             "ObjectDetectionCamera",
-            ROBOT_CENTER_TO_OBJECT_DETECTION_CAMERA
+            new DynamicCameraTransform(RobotContainer.INTAKE::calculateIntakeCameraTransformAtTime)
     );
 
     private static final StandardDeviations APRIL_TAG_CAMERA_STANDARD_DEVIATIONS = new StandardDeviations(
@@ -31,13 +22,13 @@ public class CameraConstants {
             RIGHT_TURRET_CAMERA = new AprilTagCamera(
             AprilTagCameraConstants.AprilTagCameraType.PHOTON_CAMERA,
             "RightTurretCamera",
-            new DynamicCameraTransform((timestamp) -> TurretCameraTransformCalculator.getInstance().calculateRobotToRightCameraAtTime(timestamp)),
+            new DynamicCameraTransform(RobotContainer.TURRET::calculateRightCameraTransformAtTime),
             APRIL_TAG_CAMERA_STANDARD_DEVIATIONS
     ),
             LEFT_TURRET_CAMERA = new AprilTagCamera(
                     AprilTagCameraConstants.AprilTagCameraType.PHOTON_CAMERA,
                     "LeftTurretCamera",
-                    new DynamicCameraTransform((timestamp) -> TurretCameraTransformCalculator.getInstance().calculateRobotToLeftCameraAtTime(timestamp)),
+                    new DynamicCameraTransform(RobotContainer.TURRET::calculateLeftCameraTransformAtTime),
                     APRIL_TAG_CAMERA_STANDARD_DEVIATIONS
             );
 
