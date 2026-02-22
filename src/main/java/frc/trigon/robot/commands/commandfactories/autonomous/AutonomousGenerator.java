84c3a8d3c6bfc6112387e8c2ef764a320b502c91
Trigon5990 <Trigon5990@gmail.com>
2026-02-22 21:26:19 +0200

    started implementing
    Co-Authored-By: Nummun14 <142012009+Nummun14@users.noreply.github.com>

--- src/main/java/frc/trigon/robot/commands/commandfactories/autonomous/AutonomousGenerator.java
+++ src/main/java/frc/trigon/robot/commands/commandfactories/autonomous/AutonomousGenerator.java
@@ -1,12 +1,14 @@
 package frc.trigon.robot.commands.commandfactories.autonomous;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.trigon.lib.utilities.flippable.FlippablePose2d;
+import frc.trigon.lib.utilities.flippable.FlippableTranslation2d;
 import frc.trigon.robot.RobotContainer;
 import frc.trigon.robot.constants.AutonomousConstants;
 import frc.trigon.robot.constants.FieldConstants;
@@ -15,6 +17,8 @@ import frc.trigon.robot.subsystems.swerve.SwerveCommands;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 import org.littletonrobotics.junction.networktables.LoggedNetworkBoolean;
+
+import java.util.function.Supplier;
 
 public class AutonomousGenerator {
     public static final LoggedDashboardChooser<AutonomousState>
@@ -45,28 +49,45 @@ public class AutonomousGenerator {
 
     private static Command getAutonomousStateSequenceCommand() {
         return new SequentialCommandGroup(
-                getCommandFromState(FIRST_AUTONOMOUS_CHOOSER.get(), null, SECOND_AUTONOMOUS_CHOOSER.get()),
-                getCommandFromState(SECOND_AUTONOMOUS_CHOOSER.get(), FIRST_AUTONOMOUS_CHOOSER.get(), THIRD_AUTONOMOUS_CHOOSER.get()),
-                getCommandFromState(THIRD_AUTONOMOUS_CHOOSER.get(), SECOND_AUTONOMOUS_CHOOSER.get(), FOURTH_AUTONOMOUS_CHOOSER.get()),
+                getCommandFromState(FIRST_AUTONOMOUS_CHOOSER.get(), null, SECOND_AUTONOMOUS_CHOOSER.get(), THIRD_AUTONOMOUS_CHOOSER.get(), FOURTH_AUTONOMOUS_CHOOSER.get(), FIFTH_AUTONOMOUS_CHOOSER.get()),
+                getCommandFromState(SECOND_AUTONOMOUS_CHOOSER.get(), FIRST_AUTONOMOUS_CHOOSER.get(), THIRD_AUTONOMOUS_CHOOSER.get(), FOURTH_AUTONOMOUS_CHOOSER.get(), FIFTH_AUTONOMOUS_CHOOSER.get()),
+                getCommandFromState(THIRD_AUTONOMOUS_CHOOSER.get(), SECOND_AUTONOMOUS_CHOOSER.get(), FOURTH_AUTONOMOUS_CHOOSER.get(), FIFTH_AUTONOMOUS_CHOOSER.get()),
                 getCommandFromState(FOURTH_AUTONOMOUS_CHOOSER.get(), THIRD_AUTONOMOUS_CHOOSER.get(), FIFTH_AUTONOMOUS_CHOOSER.get()),
-                getCommandFromState(FIFTH_AUTONOMOUS_CHOOSER.get(), FOURTH_AUTONOMOUS_CHOOSER.get(), null)
+                getCommandFromState(FIFTH_AUTONOMOUS_CHOOSER.get(), FOURTH_AUTONOMOUS_CHOOSER.get())
         );
     }
 
-    private static Command getCommandFromState(AutonomousState state, AutonomousState previousState, AutonomousState nextState) {
+    private static Command getCommandFromState(AutonomousState state, AutonomousState previousState, AutonomousState... nextStates) {
         if (state == null)
             return Commands.none();
 
         return switch (state) {
             case DELIVERY ->
-                    GeneralAutonomousCommands.getDeliveryCommand(previousState, AutonomousConstants.DELIVERY_TIMEOUT_SECONDS::get);
+                    GeneralAutonomousCommands.getDeliveryCommand(previousState, () -> getDeliveryTimeout(nextStates == null ? new AutonomousState[]{} : nextStates));
             case SCORE ->
-                    GeneralAutonomousCommands.getScoreCommand(nextState, AutonomousConstants.SCORING_TIMEOUT_SECONDS);
+                    GeneralAutonomousCommands.getScoreCommand(nextStates == null ? null : nextStates[0], AutonomousConstants.SCORING_TIMEOUT_SECONDS);
             case COLLECT_FROM_DEPOT ->
                     GeneralAutonomousCommands.getCollectFromDepotCommand(true, AutonomousConstants.DEPOT_COLLECTION_TIMEOUT_SECONDS);
             case COLLECT_FROM_NEUTRAL_ZONE ->
                     GeneralAutonomousCommands.getCollectFromNeutralZoneCommand(previousState, AutonomousConstants.NEUTRAL_ZONE_COLLECTION_TIMEOUT_SECONDS);
         };
+    }
+
+    private static double getDeliveryTimeout(AutonomousState[] nextStates) {
+        double timeToLeave = 0;
+        for (AutonomousState nextState : nextStates) {
+            switch (nextState) {
+                case SCORE -> timeToLeave += AutonomousConstants.SCORING_TIMEOUT_SECONDS;
+                case COLLECT_FROM_DEPOT -> timeToLeave += AutonomousConstants.DEPOT_COLLECTION_TIMEOUT_SECONDS;
+                case COLLECT_FROM_NEUTRAL_ZONE -> timeToLeave += AutonomousConstants.NEUTRAL_ZONE_COLLECTION_TIMEOUT_SECONDS;
+            }
+        }
+
+        if (shouldClimb())
+            timeToLeave += calculateTimeToLeaveForClimbSeconds(nextStates.length == 0 ? AutonomousState.DELIVERY.expectedRobotPose.get().get() : nextStates[nextStates.length - 1].expectedRobotPose.get().get()) + 0.5;
+
+        // implement logic to deal with earlier states time
+        return 0;
     }
 
     private static Command getLogCommand() {
@@ -81,15 +102,18 @@ public class AutonomousGenerator {
     }
 
     private static boolean shouldStartDrivingToClimb() {
-        if (!shouldClimb() || !IS_AUTONOMOUS_CLIMB_HIGHEST_PRIORITY.get())
-            return false;
-
-        final Pose2d currentRobotPose = RobotContainer.ROBOT_POSE_ESTIMATOR.getEstimatedRobotPose();
-        final Pose2d targetClimbPose = CLIMB_POSITION_CHOOSER.get().climbPose.get();
-        final double distanceToClimbPoseMeters = currentRobotPose.getTranslation().getDistance(targetClimbPose.getTranslation());
-        final double estimatedDriveTimeSeconds = distanceToClimbPoseMeters / AutonomousConstants.ROBOT_AVERAGE_SPEED_METERS_PER_SECOND;
-        final double timeToLeaveForClimbSeconds = AutonomousConstants.ESTIMATED_CLIMBING_TIME_SECONDS + estimatedDriveTimeSeconds + AutonomousConstants.CLIMB_DRIVE_TIME_SAFETY_MARGIN_SECONDS;
+        final double timeToLeaveForClimbSeconds = calculateTimeToLeaveForClimbSeconds(RobotContainer.ROBOT_POSE_ESTIMATOR.getEstimatedRobotPose().getTranslation());
         return MatchTracker.getMatchTimeSeconds() <= AutonomousConstants.TOTAL_MATCH_TIME_SECONDS - AutonomousConstants.AUTONOMOUS_TIME_SECONDS + timeToLeaveForClimbSeconds;
+    }
+
+    private static double calculateTimeToLeaveForClimbSeconds(Translation2d robotPose) {
+        if (!shouldClimb() || !IS_AUTONOMOUS_CLIMB_HIGHEST_PRIORITY.get())
+            return 0;
+
+        final Pose2d targetClimbPose = CLIMB_POSITION_CHOOSER.get().climbPose.get();
+        final double distanceToClimbPoseMeters = robotPose.getDistance(targetClimbPose.getTranslation());
+        final double estimatedDriveTimeSeconds = distanceToClimbPoseMeters / AutonomousConstants.ROBOT_AVERAGE_SPEED_METERS_PER_SECOND;
+        return AutonomousConstants.ESTIMATED_CLIMBING_TIME_SECONDS + estimatedDriveTimeSeconds + AutonomousConstants.CLIMB_DRIVE_TIME_SAFETY_MARGIN_SECONDS;
     }
 
     public static boolean shouldClimb() {
@@ -112,15 +136,17 @@ public class AutonomousGenerator {
     }
 
     public enum AutonomousState {
-        DELIVERY(false),
-        SCORE(true),
-        COLLECT_FROM_DEPOT(true),
-        COLLECT_FROM_NEUTRAL_ZONE(false);
+        DELIVERY(false, () -> SafeAutonomousDriveCommands.isRight() ? FieldConstants.RIGHT_DELIVERY_POSITION : FieldConstants.LEFT_DELIVERY_POSITION),
+        SCORE(true, () -> SafeAutonomousDriveCommands.isRight() ? FieldConstants.RIGHT_IDEAL_SHOOTING_POSITION.getTranslation() : FieldConstants.LEFT_IDEAL_SHOOTING_POSITION.getTranslation()),
+        COLLECT_FROM_DEPOT(true, FieldConstants.DEPOT_POSITION::getTranslation),
+        COLLECT_FROM_NEUTRAL_ZONE(false, () -> SafeAutonomousDriveCommands.isRight() ? FieldConstants.RIGHT_INTAKE_POSITION.getTranslation() : FieldConstants.LEFT_INTAKE_POSITION.getTranslation());
 
         final boolean isInAllianceZone;
+        final Supplier<FlippableTranslation2d> expectedRobotPose;
 
-        AutonomousState(boolean isInAllianceZone) {
+        AutonomousState(boolean isInAllianceZone, Supplier<FlippableTranslation2d> expectedRobotPose) {
             this.isInAllianceZone = isInAllianceZone;
+            this.expectedRobotPose = expectedRobotPose;
         }
     }
 
