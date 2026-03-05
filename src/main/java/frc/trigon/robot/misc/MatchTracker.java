381a1ee7665241665ceea8a6d8a7bef715bff024
NoamB <shmaya.rosenblatt@gmail.com>
2026-03-06 00:52:38 +0200

    initial commit

--- src/main/java/frc/trigon/robot/misc/MatchTracker.java
+++ src/main/java/frc/trigon/robot/misc/MatchTracker.java
@@ -1,11 +1,34 @@
 package frc.trigon.robot.misc;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
 import frc.trigon.lib.utilities.flippable.Flippable;
 import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.networktables.LoggedNetworkBoolean;
 
 public final class MatchTracker {
+    private static final LoggedNetworkBoolean OVERRIDE_IS_HUB_ACTIVE = new LoggedNetworkBoolean("MatchTracker/OverrideIsHubActive", false);
     private static final double TIME_BEFORE_ALLIANCE_SHIFT_TO_INDICATE_SECONDS = 5;
+    private static final double HUB_DEACTIVATION_TIME_SECONDS = 3;
+    private static final double MINIMUM_FUEL_DETECTION_DELAY = 1;
+    private static final double MAXIMUM_FUEL_DETECTION_DELAY = 2;
+    private static final double FUEL_FLIGHT_TIME_SECONDS = 1.5;
+    private static double startingMatchTime;
+
+    public static void setStartingMatchTime() {
+        startingMatchTime = Timer.getFPGATimestamp();
+    }
+
+    @AutoLogOutput(key = "MatchTracker/CurrentShift")
+    public static String getCurrentShiftName() {
+        if (getMatchTimeSeconds() <= 20)
+            return "Autonomous";
+        if (getMatchTimeSeconds() <= 30)
+            return "Transition Shift";
+        if (getMatchTimeSeconds() <= 55)
+            return "Shift 1";
+        return "";
+    }
 
     @AutoLogOutput(key = "Assists/ShouldIndicateAllianceShift")
     public static boolean shouldIndicateAllianceShift() {
@@ -13,14 +36,22 @@ public final class MatchTracker {
                 isHubActive(getMatchTimeSeconds() - TIME_BEFORE_ALLIANCE_SHIFT_TO_INDICATE_SECONDS);
     }
 
-    @AutoLogOutput(key = "IsHubActive")
+    @AutoLogOutput(key = "MatchTracker/IsHubActive")
     public static boolean isHubActive() {
-        return isHubActive(getMatchTimeSeconds());
+        return isHubActive(getMatchTimeSeconds()) || OVERRIDE_IS_HUB_ACTIVE.get();
     }
 
     public static boolean isHubActive(double matchTimeSeconds) {
         if (!DriverStation.isTeleop())
             return true;
+
+        if (!isHubActive(matchTimeSeconds)) {
+            final int currentShift = getShiftNumber(matchTimeSeconds);
+            if (getTimeUntilShiftEnd(currentShift) <= MINIMUM_FUEL_DETECTION_DELAY + FUEL_FLIGHT_TIME_SECONDS)
+                return true;
+            if (getTimeSinceShiftStart(currentShift) + MAXIMUM_FUEL_DETECTION_DELAY + FUEL_FLIGHT_TIME_SECONDS - HUB_DEACTIVATION_TIME_SECONDS <= 0)
+                return true;
+        }
 
         final boolean isRedAlliance = Flippable.isRedAlliance();
         final String gameMessage = DriverStation.getGameSpecificMessage();
@@ -31,8 +62,9 @@ public final class MatchTracker {
         return isRedAlliance == isRedHubActive;
     }
 
+
     public static double getMatchTimeSeconds() {
-        return DriverStation.getMatchTime();
+        return Timer.getFPGATimestamp() - startingMatchTime;
     }
 
     private static boolean isRedHubActive(boolean didRedAllianceWinAutonomous, double matchTimeSeconds) {
@@ -46,15 +78,37 @@ public final class MatchTracker {
         return true;
     }
 
+    private static double getTimeUntilShiftEnd(int shift) {
+        return getShiftEndTimeStamp(shift) - getMatchTimeSeconds();
+    }
+
+    private static double getTimeSinceShiftStart(int shift) {
+        return getMatchTimeSeconds() - getShiftStartTimeStamp(shift);
+    }
+
+    private static double getShiftEndTimeStamp(int shift) {
+        return getShiftStartTimeStamp(shift) + 25;
+    }
+
+    private static double getShiftStartTimeStamp(int shift) {
+        return switch (shift) {
+            case 1 -> 30;
+            case 2 -> 55;
+            case 3 -> 80;
+            case 4 -> 105;
+            default -> 0;
+        };
+    }
+
     private static int getShiftNumber(double matchTimeSeconds) {
         if (matchTimeSeconds > 30 && matchTimeSeconds <= 55)
-            return 4;
-        if (matchTimeSeconds > 55 && matchTimeSeconds <= 80)
-            return 3;
-        if (matchTimeSeconds > 80 && matchTimeSeconds <= 105)
-            return 2;
-        if (matchTimeSeconds > 105 && matchTimeSeconds <= 130)
             return 1;
+        if (matchTimeSeconds > 55 && matchTimeSeconds <= 80)
+            return 2;
+        if (matchTimeSeconds > 80 && matchTimeSeconds <= 105)
+            return 3;
+        if (matchTimeSeconds > 105 && matchTimeSeconds <= 130)
+            return 4;
         return -1;
     }
 }
