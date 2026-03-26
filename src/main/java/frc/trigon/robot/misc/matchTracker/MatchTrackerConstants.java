adcdb7fa97c963d78e54fb5feb48f78c4cdb5db8
ShmayaR <shmaya.rosenblatt@gmail.com>
2026-03-26 15:04:57 +0200

    Shift Tracker (#57)
    * initial commit
    
    * Update MatchTracker.java
    
    * logic fix
    
    * Update MatchTracker.java
    
    * made getTimeSinceLastAllianceShiftSeconds private
    
    * removed redundant if statement in getTimeUntilAllianceShiftSeconds
    
    * created match tracker constants class
    
    * update import
    
    * removed autolog for hub active and indicate alliance shift
    
    * this is what Jonah said Shmaya
    
    Co-Authored-By: Nummun14 <142012009+Nummun14@users.noreply.github.com>
    
    * reformat
    
    * Simplify logic and cleanup
    
    ---------
    
    Co-authored-by: Trigon5990 <Trigon5990@gmail.com>
    Co-authored-by: Nummun14 <142012009+Nummun14@users.noreply.github.com>

---
+++ src/main/java/frc/trigon/robot/misc/matchTracker/MatchTrackerConstants.java
@@ -0,0 +1,11 @@
+package frc.trigon.robot.misc.matchTracker;
+
+public class MatchTrackerConstants {
+    static final double
+            ALLIANCE_SHIFT_DURATION_SECONDS = 25,
+            TIME_BEFORE_ALLIANCE_SHIFT_TO_INDICATE_SECONDS = 5,
+            HUB_DEACTIVATION_TIME_SECONDS = 3,
+            MINIMUM_FUEL_DETECTION_DELAY = 1,
+            MAXIMUM_FUEL_DETECTION_DELAY = 2,
+            FUEL_FLIGHT_TIME_SECONDS = 1.5;
+}
