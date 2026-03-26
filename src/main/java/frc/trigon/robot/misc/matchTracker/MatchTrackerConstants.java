cbbd70a9e79c995514619195965b8fcef5ab047b
Trigon5990 <Trigon5990@gmail.com>
2026-03-26 15:10:30 +0200

    Update MatchTrackerConstants.java

--- src/main/java/frc/trigon/robot/misc/matchTracker/MatchTrackerConstants.java
+++ src/main/java/frc/trigon/robot/misc/matchTracker/MatchTrackerConstants.java
@@ -3,7 +3,7 @@ package frc.trigon.robot.misc.matchTracker;
 public class MatchTrackerConstants {
     static final double
             ALLIANCE_SHIFT_DURATION_SECONDS = 25,
-            TIME_BEFORE_ALLIANCE_SHIFT_TO_INDICATE_SECONDS = 5,
+            TIME_BEFORE_ALLIANCE_SHIFT_TO_INDICATE_SECONDS = 8,
             HUB_DEACTIVATION_TIME_SECONDS = 3,
             MINIMUM_FUEL_DETECTION_DELAY = 1,
             MAXIMUM_FUEL_DETECTION_DELAY = 2,
